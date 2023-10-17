9318cab4d48106cb5ec585ea263f7912d8f91434
Juan Jose Ponce Moreno <105078567+Utm20040062@users.noreply.github.com>
2023-10-17 00:00:39 +0000

    Se creo las carpetas routes y middleware

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import bcrypt from 'bcrypt';
 import './env.js';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import router from './routes/router.js';
+import verifyApiKey from './middlewares/verifyApiKey.js';
 // TODO: Add SDKs for Firebase products that you want to use
 
 const app = express();
@@ -35,11 +36,62 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api', router);
+
+app.use(verifyApiKey);
+
 // routes
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
+app.post('/register', async (req, res) => {
+  const { password } = req.body;
+  // Using callbak
+  // bcrypt.genSalt(saltRounds, function (err, salt) {
+  //   bcrypt.hash(password, salt, function (error, hashPassword) {
+  //     hash = hashPassword;
+  //     console.log(hash);
+  //     res.send(hash);
+  //   });
+  // });
+
+  // Using async await (recommended)
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  res.send(hashedPassword);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log(
+    req.body,
+    `select email, password from users where email='${email}' and password='${password}'`
+  );
+  // without mysql.escape
+  // connection.query(
+  //   `select email, password from users where email='${email}' and password='${password}'`,
+  //   (err, results, fields) => {
+  //     if (err) {
+  //       return res.status(500).send(err);
+  //     }
+  //     console.log(results);
+  //     return res.send(results);
+  //   }
+  // );
+  // with mysql.escape
+  //   connection.query(
+  //   `select email, password from users where email=${mysql.escape(
+  //       email
+  //     )} and password=${mysql.escape(password)}`,
+  //     (err, results, fields) => {
+  //       if (err) {
+  //         return res.status(500).send(err);
+  //       }
+  //       console.log(results);
+  //       return res.send(results);
+  //     }
+  //   );
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
