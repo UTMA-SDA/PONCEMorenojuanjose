79d220790817079f0f1a6535810c15e3f4e5bc9f
Juan Jose Ponce Moreno <105078567+Utm20040062@users.noreply.github.com>
2023-10-31 23:32:01 +0000

    Se añadio verify api key hacia router

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import auth from './auth.js';
-
+import verifyApiKey from "../middlewares/verifyApiKey.js";
 const router = express.Router();
 
 router.use('/auth', auth);
+
+router.use(verifyApiKey);
 
 export default router;
