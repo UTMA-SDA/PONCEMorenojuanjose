265db0320d324ede17ea209f61cc078a088b9f74
Juan Jose Ponce Moreno <105078567+Utm20040062@users.noreply.github.com>
2023-11-06 21:42:55 +0000

    sE CREO API personal

--- routes/ponmor.js
+++ routes/ponmor.js
@@ -5,23 +5,20 @@ const router = express.Router();
 router.get('/', (req, res) => {
   async function query(data) {
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0',
+      "https://api-inference.huggingface.co/models/segmind/SSD-1B",
       {
-        headers: {
-          Authorization: 'Bearer ' + process.env.HF_ACCESS_TOKEN,
-        },
-        method: 'POST',
+        headers: { Authorization: "Bearer "+ process.env.HF_ACCESS_TOKEN },
+        method: "POST",
         body: JSON.stringify(data),
       }
     );
     const result = await response.blob();
     return result;
   }
-  query({ inputs: 'A cat with a halloween hat' }).then((response) => {
-    // Use image
+  query({"inputs": "Astronaut riding a horse"}).then((response) => {
     res.type(response.type);
-    response.arrayBuffer().then((buffer) => {
-      res.send(Buffer.from(buffer));
+    response.arrayBuffer().then((buffer)=>{
+      res.send(Buffer.from(buffer))
     });
   });
 });
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from './auth.js';
 import verifyApiKey from "../middlewares/verifyApiKey.js";
+import ponmor from './ponmor.js';
 const router = express.Router();
 
 router.use('/auth', auth);
